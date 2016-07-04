1b319d88d9508ce3d8237b9fa45e7e15cbb223e5
jixnul <ahmadgivekesh@gmail.com>
2016-07-04 13:15:54 +0430

    comments and post_by_url added.

--- custom_rest_api.php
+++ custom_rest_api.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Custom Rest API
 Description: DO NOT DISABLE/REMOVE THIS... this plugin is essential for android app!
-Version:     1.1
+Version:     1.2
 Author:      Ahmad Givekesh
 Author URI:  baboon.ir
 License:     Apache v2.0
@@ -24,6 +24,34 @@ add_action( 'rest_api_init', function () {
 			'id' => array(
 				'validate_callback' => function($param, $request, $key) {
 					return is_numeric( $param );
+				}
+			),
+		),
+	) );
+} );
+
+add_action( 'rest_api_init', function () {
+	register_rest_route( 'givekesh', '/comments/(?P<id>\d+)', array(
+		'methods' => 'GET',
+		'callback' => 'get_comments_by_id',
+		'args' => array(
+			'id' => array(
+				'validate_callback' => function($param, $request, $key) {
+					return is_numeric( $param );
+				}
+			),
+		),
+	) );
+} );
+
+add_action( 'rest_api_init', function () {
+	register_rest_route( 'givekesh', '/post_by_url/(?P<url>.+)', array(
+		'methods' => 'GET',
+		'callback' => 'get_post_by_url',
+		'args' => array(
+			'url' => array(
+				'validate_callback' => function($param, $request, $key) {
+					return preg_match("/http?:\/\/(www\.)?baboon.ir\/([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/", $param);
 				}
 			),
 		),
@@ -91,4 +119,34 @@ function my_trim_excerpt( $text, $length = 55, $more = ' [&hellip;]'  ){
 
      $excerpt = wp_trim_words( $text, $length, $more );
      return $excerpt;
+}
+
+function get_comments_by_id($data){
+	$comments = get_comments(array(
+		'post_id' => $data['id'],
+		'orderby' => 'comment_date',
+		'order' => 'ASC'));
+		
+	if(empty($comments))
+		return new WP_Error( 'OPS...', 'Something Went Wrong In Our End! Please Try Later.', array( 'status' => 404 ) );
+
+	$i=0;
+	foreach($comments as $comment){
+		if($comment-> comment_approved){
+			$result[$i]['id'] = $comment->comment_ID;
+			$result[$i]['parent'] = $comment->comment_parent;
+			$result[$i]['date'] = $comment->comment_date;
+			$result[$i]['content'] = $comment-> comment_content;
+			$result[$i++]['author_info'] = array(
+			"display_name" => $comment->comment_author,
+			"avatar_url"=> get_avatar_url($comment->comment_author_email));
+		}
+	}
+	
+	return $result;
+}
+
+function get_post_by_url($data){
+	$url = array('id' => url_to_postid($data['url']));
+	return get_post_by_id($url);
 }
